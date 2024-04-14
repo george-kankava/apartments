59c3d768e2e79aaf9eba5e4e8550de6c66256053
george-kankava <george.kankava@gmail.com>
2024-04-14 11:22:37 +0400

    Second commit. (#2)
    Co-authored-by: George Kankava <gkankava@uplandsoftware.com>

--- src/main/java/com/apart/ge/apartments/repository/PropertyRepository.java
+++ src/main/java/com/apart/ge/apartments/repository/PropertyRepository.java
@@ -2,8 +2,13 @@ package com.apart.ge.apartments.repository;
 
 
 import com.apart.ge.apartments.enums.PropertyType;
+import com.apart.ge.apartments.model.Property;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-public class PropertyRepository {
+@Repository
+public interface PropertyRepository extends CrudRepository<Property, Integer> {
 
 
+    public Property getProperty(Integer id, PropertyType propertyType);
 }
