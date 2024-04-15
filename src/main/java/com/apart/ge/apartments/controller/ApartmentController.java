0bcac52d124c47545b44cb8774dea4f7f788aea8
George Kankava <gkankava@uplandsoftware.com>
2024-04-15 21:37:28 +0400

    Second commit.

--- src/main/java/com/apart/ge/apartments/controller/ApartmentController.java
+++ src/main/java/com/apart/ge/apartments/controller/ApartmentController.java
@@ -3,9 +3,13 @@ package com.apart.ge.apartments.controller;
 import com.apart.ge.apartments.model.Greeting;
 import com.apart.ge.apartments.model.Property;
 import com.apart.ge.apartments.service.ApartmentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,5 +43,20 @@ public class ApartmentController {
     @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+    @GetMapping(value = "/{id}", produces = "application/json")
+    public ResponseEntity<Property> getById(@PathVariable("id") Integer id) {
+        Property apartment = apartmentService.getApartment(id);
+        if (apartment != null) {
+            return ResponseEntity.ok(apartment);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    @PostMapping(value = "/", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<Property> createProperty(@RequestBody Property property) {
+        Property createdProperty = apartmentService.createProperty(property);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdProperty);
     }
 }
